c83ffb138e1e684eb1c3d183258350c2a2e5a5b8
Arthur Mak <arthur.t.mak@gmail.com>
2015-12-04 18:28:48 +0800

    added hovering layers

--- Website2/js/main-code.js
+++ Website2/js/main-code.js
@@ -1,14 +1,16 @@
 
 // $("#sl1").sparkline([5,6,7,9,9,5,3,2,2,4,6,7,1,2,2,5,4,2,21,7,8], {type: 'line'}); 
 
-var modes = ['walk', 'bike', 'car', 'taxi', 'bus', 'subway'];
+// var modes = ['walk', 'bike', 'car', 'taxi', 'bus', 'subway']; 
+
 var seasons = ['2008-2', '2008-3', '2008-4', '2009-1', '2009-2', '2009-3'];
 var mode_colors = {'walk': '#fbb4ae', 'bike': '#b3cde3', 'car': '#ccebc5', 'taxi': '#decbe4', 'bus': '#fed9a6', 'subway': '#ffffcc'};
 var season_index = 0;
 display();
 plot_xy(data_xy[seasons[season_index]]);  //REMOVE LATER
 
-create_map(data_xy[seasons[season_index]]);
+// create_map(data_xy[seasons[season_index]]);
+create_map(data_xy[seasons[season_index]], true, modes);
 
 function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -27,7 +29,8 @@ function next() {
   display();
   plot_xy(data_xy[seasons[season_index]]); //REMOVE LATER
 
-  create_map(data_xy[seasons[season_index]])
+// create_map(data_xy[seasons[season_index]]);
+  create_map(data_xy[seasons[season_index]], false, modes);
 
 }
 
@@ -44,7 +47,8 @@ function back() {
   display();
   plot_xy(data_xy[seasons[season_index]]); //REMOVE LATER
 
-  create_map(data_xy[seasons[season_index]])
+// create_map(data_xy[seasons[season_index]]);
+  create_map(data_xy[seasons[season_index]], false, modes);
 
 }
 
@@ -67,7 +71,8 @@ function update() {
   display();
   plot_xy(data_xy[seasons[season_index]]); //REMOVE LATER
 
-  create_map(data_xy[seasons[season_index]])
+// create_map(data_xy[seasons[season_index]]);
+  create_map(data_xy[seasons[season_index]], false, modes);
 
 }
 
@@ -97,7 +102,11 @@ function display() {
       hourly = '';
     }
 
-    html = '<tr><td> <i class="fa fa-circle" style="color:' + mode_colors[mode] + '"></i> ' + capitalize(mode) + '</td>';
+    //arthur changes START
+    html = '<tr class="row-mode" id='+mode+'><td> <i class="fa fa-circle" style="color:' + mode_colors[mode] + '"></i> ' + capitalize(mode) + '</td>';
+    // html = '<tr><td> <i class="fa fa-circle" style="color:' + mode_colors[mode] + '"></i> ' + capitalize(mode) + '</td>';
+    // arthur changes END
+
     html += '<td><div class="chart"><div style="width: ' + path_pct + 'px"></div></div></td>';
     html += '<td style="text-align:right">' + avg_dist + '</td>';
     html += '<td style="text-align:right">' + avg_dur + '</td><td><span class="inlineline">' + hourly + '</span></td></tr>';
--- temp.js
+++ temp.js
@@ -1,28 +1,50 @@
 var map = L.map('leaflet-map').setView([39.952, 116.370], 11);
 var overlayMaps={};
 
+// MUST CHANGE....
+var modes = ['walk', 'bike', 'car', 'taxi', 'bus', 'subway'];
 
-// var modes = ['walk', 'bike', 'car', 'taxi', 'bus', 'subway']; 
+$(".mode").click(function(event) {
+    event.preventDefault();
+    if(map.hasLayer(overlayMaps[this.id])) {
+        $(this).removeClass('selected');
+        map.removeLayer(this.id);
+    } else {
+        map.addLayer(overlayMaps[this.id]);        
+        $(this).addClass('selected');
+   }
+   console.log('hello')
+});
 
-//select different transport mode layer as hover over different modes
-//http://stackoverflow.com/questions/12853622/hover-callback-function-with-on-and-binding
-$('#tbl').on({
-  mouseenter: function(){
-    for (i in modes){
-      if (modes[i]!=this.id){
-        map.removeLayer(overlayMaps[modes[i]]);
-      }
-    }
-  },
-  mouseleave: function(){
-    for (i in modes){
-      if (modes[i]!=this.id){
-        map.addLayer(overlayMaps[modes[i]]);
-      }
-    }
-  } 
-}, '.row-mode');
 
+
+
+
+// $(".mode").hover(function(event) { //mouse hover-in
+//   // event.preventDefault();
+//   // if(map.hasLayer(overlayMaps[this.id])) {
+//     // $(this).removeClass('selected');
+//     console.log(overlayMaps['walk']);
+//     map.removeLayer(overlayMaps['walk']);
+
+//     for (i in modes){
+//       // console.log(modes[i]);
+//       // console.log(this.id);
+//       if (modes[i]!=this.id){
+//         map.removeLayer(overlayMaps[modes[i]]);
+//         map.removeLayer(overlayMaps[this.id]);
+//         // console.log(map);
+//       }
+//     // }
+
+//   }}, function(event){ //mouse hover-away
+//     console.log('hover-away');
+
+//   // else {
+//   //   map.addLayer(overlayMaps[this.id]);        
+//   //   $(this).addClass('selected');
+//   // }
+// });
 
 //main function for populating map objects
 function create_map (data, new_map, transports){
@@ -30,23 +52,18 @@ function create_map (data, new_map, transports){
   // console.log(data.length);
   // console.log(data);
 
-  //remove exsiting data if only updating map (not creating new map)
-  if (!new_map){
-    for (i in modes){
-      if (modes[i]!=this.id){
-        map.removeLayer(overlayMaps[modes[i]]);
-      }
-    }
-    overlayMaps={};
-  }
-
-  //plot all paths from seasonal data
   for (i in data){
+
     dataXY = data[i].data;
     mode_transport = data[i].name
     var paths = [];  //for storing the array of polylines
 
+    // console.log(transports);
+    // console.log(transport_data);
+    // console.log($.inArray(transport_data, transports)> -1)
+
     //http://stackoverflow.com/questions/24658596/hide-show-layergroups-in-leaflet-with-own-buttons
+
     if ($.inArray(mode_transport, transports)> -1){
       var pointList = [];
 
@@ -55,7 +72,9 @@ function create_map (data, new_map, transports){
         var y = dataXY[j][1];
         // console.log(x);
         // console.log(y);
+
         pointList.push(L.latLng(x,y));
+
       }
 
       path = L.polyline(pointList, {
@@ -71,10 +90,41 @@ function create_map (data, new_map, transports){
 
     //add layer for each transport mode
     overlayMaps[mode_transport] = L.layerGroup(paths);
-    map.addLayer(overlayMaps[mode_transport]);
-    // L.layerGroup(paths).addTo(map);
+    L.layerGroup(paths).addTo(map);
     // console.log(overlayMaps);
   }
+
+  // L.control.layers(overlayMaps).addTo(map); 
+
+
+  //   if ($.inArray(transport_mode, transports)> -1){
+  //     var pointList = [];
+
+  //     for (j in dataXY){
+  //       var x = dataXY[j][0];
+  //       var y = dataXY[j][1];
+  //       // console.log(x);
+  //       // console.log(y);
+
+  //       pointList.push(L.latLng(x,y));
+
+  //     }
+
+  //     var path = L.polyline(pointList, {
+  //       // color: 'blue',
+  //       color: data[i].color,
+  //       weight: 1,
+  //       opacity: 0.5,
+  //       smoothFactor: 1
+
+  //     });
+  //     path.addTo(map);
+
+  //   }
+  // }
+
+
+
 
 
   // API call to mapbox.com
